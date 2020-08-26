89963d426e703dd1b42e2e4fb8a1247f95c5e985
Mikael N <uusikauttaja02022020@outlook.com>
2020-08-26 09:33:22 +0300

    Init project and auto generated menu

---
+++ Exercises/IExercise.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseApp3
+{
+    interface IExercise
+    {
+        public void Run();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace ExerciseApp3
+{
+    class Program
+    {
+        private static bool exit = false;
+
+        static void Main(string[] args)
+        {
+            LoadUsingAppDomain();
+        }
+
+        static void LoadUsingAppDomain()
+        {
+            var type = typeof(IExercise);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                        .SelectMany(s => s.GetTypes())
+                        .Where(p => type.IsAssignableFrom(p) && p.IsClass);
+
+            while (!exit)
+                PrintLoadedMainMenu(types);
+        }
+
+        static string GetDescriptionAttributeText(Type t)
+        {
+            string result = "";
+            DescriptionAttribute descriptionAttribute = (DescriptionAttribute)t.GetCustomAttribute(typeof(DescriptionAttribute));
+            result = descriptionAttribute?.Description;
+
+            return result;
+        }
+
+        static void PrintLoadedMainMenu(IEnumerable<Type> exercises)
+        {
+            Console.Clear();
+            Console.WriteLine("Select Exercise to run:");
+            Console.WriteLine("-----------------------");
+
+            Type[] exerArray = exercises.ToArray();
+
+            for (int i = 0; i < exercises.Count(); i++)
+            {
+                Type t = exerArray[i];
+
+                Console.WriteLine($"{i}) {t.Name} - {GetDescriptionAttributeText(t)}");
+            }
+
+            char key = Console.ReadKey(true).KeyChar;
+            int selection;
+
+            if (int.TryParse(key.ToString(), out selection))
+            {
+                if (selection >= exerArray.Length)
+                    return; // selection out of range
+
+                IExercise selectedExer = Activator.CreateInstance(exerArray[selection]) as IExercise;
+                selectedExer.Run();
+
+                // Stop returning 
+                Console.ReadKey();
+            }
+        }
+    }
+}
